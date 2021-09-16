71c84443ac2cf8f379f2872e6086d0b6e147953a
Brian Kim <briankimpossible@gmail.com>
2021-09-16 17:13:03 -0400

    attempt to reproduce veevo useLazyQuery bug

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,10 @@ import {
   GraphQLID,
   GraphQLString,
   GraphQLList,
-} from 'graphql';
+} from "graphql";
+
 const PersonType = new GraphQLObjectType({
-  name: 'Person',
+  name: "Person",
   fields: {
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -15,54 +16,46 @@ const PersonType = new GraphQLObjectType({
 });
 
 const peopleData = [
-  { id: 1, name: 'John Smith' },
-  { id: 2, name: 'Sara Smith' },
-  { id: 3, name: 'Budd Deey' },
+  { id: "1", name: "John Smith" },
+  { id: "2", name: "Sara Smith" },
+  { id: "3", name: "Budd Deey" },
 ];
 
 const QueryType = new GraphQLObjectType({
-  name: 'Query',
+  name: "Query",
   fields: {
     people: {
       type: new GraphQLList(PersonType),
       resolve: () => peopleData,
     },
+
+		person: {
+			type: PersonType,
+			args: {
+				id: { type: GraphQLID },
+			},
+			resolve: function(_, { id }) {
+				return peopleData.find((person) => (person.id === id));
+			},
+		},
   },
 });
 
-const MutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {
-    addPerson: {
-      type: PersonType,
-      args: {
-        name: { type: GraphQLString },
-      },
-      resolve: function (_, { name }) {
-        const person = {
-          id: peopleData[peopleData.length - 1].id + 1,
-          name,
-        };
-
-        peopleData.push(person);
-        return person;
-      }
-    },
-  },
+const schema = new GraphQLSchema({
+	query: QueryType,
 });
-
-const schema = new GraphQLSchema({ query: QueryType, mutation: MutationType });
 
 /*** LINK ***/
 import { graphql, print } from "graphql";
 import { ApolloLink, Observable } from "@apollo/client";
 function delay(wait) {
-  return new Promise(resolve => setTimeout(resolve, wait));
+  return new Promise((resolve) => setTimeout(resolve, wait));
 }
 
-const link = new ApolloLink(operation => {
-  return new Observable(async observer => {
+const link = new ApolloLink((operation) => {
+  return new Observable(async (observer) => {
     const { query, operationName, variables } = operation;
+    console.log("request", {query, operationName, variables});
     await delay(300);
     try {
       const result = await graphql(
@@ -71,7 +64,7 @@ const link = new ApolloLink(operation => {
         null,
         null,
         variables,
-        operationName,
+        operationName
       );
       observer.next(result);
       observer.complete();
@@ -82,15 +75,14 @@ const link = new ApolloLink(operation => {
 });
 
 /*** APP ***/
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  gql,
-  useQuery,
-  useMutation,
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache,
+	gql,
+	useLazyQuery,
 } from "@apollo/client";
 import "./index.css";
 
@@ -103,9 +95,9 @@ const ALL_PEOPLE = gql`
   }
 `;
 
-const ADD_PERSON = gql`
-  mutation AddPerson($name: String) {
-    addPerson(name: $name) {
+const GET_PERSON = gql`
+  query GetPerson($id: ID) {
+    person(id: $id) {
       id
       name
     }
@@ -113,69 +105,57 @@ const ADD_PERSON = gql`
 `;
 
 function App() {
-  const [name, setName] = useState('');
-  const {
-    loading,
-    data,
-  } = useQuery(ALL_PEOPLE);
-
-  const [addPerson] = useMutation(ADD_PERSON, {
-    update: (cache, { data: { addPerson: addPersonData } }) => {
-      const peopleResult = cache.readQuery({ query: ALL_PEOPLE });
-
-      cache.writeQuery({
-        query: ALL_PEOPLE,
-        data: {
-          ...peopleResult,
-          people: [
-            ...peopleResult.people,
-            addPersonData,
-          ],
-        },
-      });
+  const [view, setView] = useState(true);
+  const [
+    getAllPeople,
+    { loading: peopleLoading, data: peopleData },
+  ] = useLazyQuery(ALL_PEOPLE, {
+    onCompleted() {
+      console.log("ALL PEOPLE COMPLETED");
+    },
+  });
+  const [
+    getPerson,
+    { loading: personLoading, data: personData },
+  ] = useLazyQuery(GET_PERSON, {
+    variables: {id: 1},
+    onCompleted() {
+      console.log("PERSON COMPLETED");
     },
   });
 
+  useEffect(() => {
+    if (view) {
+      console.log("getAllPeople");
+      getAllPeople();
+    } else {
+      console.log("getPerson");
+      getPerson({ id: 1 });
+    }
+  }, [getAllPeople, getPerson, view]);
   return (
     <main>
       <h1>Apollo Client Issue Reproduction</h1>
       <p>
         This application can be used to demonstrate an error in Apollo Client.
       </p>
-      <div className="add-person">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={evt => setName(evt.target.value)}
-        />
-        <button
-          onClick={() => {
-            addPerson({ variables: { name } });
-            setName('');
-          }}
-        >
-          Add person
-        </button>
-      </div>
+      <button onClick={() => setView((view) => !view)}>
+        {view ? "switch to get person" : "switch to get all people"}
+      </button>
       <h2>Names</h2>
-      {loading ? (
-        <p>Loading…</p>
-      ) : (
-        <ul>
-          {data?.people.map(person => (
-            <li key={person.id}>{person.name}</li>
-          ))}
-        </ul>
-      )}
+      <div>
+        <div>peopleLoading: {JSON.stringify(peopleLoading)}</div>
+        <div>personLoading: {JSON.stringify(personLoading)}</div>
+        <div>peopleData: {JSON.stringify(peopleData)}</div>
+        <div>personData: {JSON.stringify(personData)}</div>
+      </div>
     </main>
   );
 }
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
+  link,
 });
 
 render(
