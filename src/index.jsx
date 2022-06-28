5b66ac27e1c891fdc05ff9cb47bce440558b7759
Alessia Bellisario <alessia@apollographql.com>
2022-06-28 09:24:03 -0400

    Demo useBackgroundQuery approach

--- src/index.jsx
+++ src/index.jsx
@@ -1,183 +1,129 @@
-/*** SCHEMA ***/
-import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLList,
-} from 'graphql';
-const PersonType = new GraphQLObjectType({
-  name: 'Person',
-  fields: {
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-  },
-});
-
-const peopleData = [
-  { id: 1, name: 'John Smith' },
-  { id: 2, name: 'Sara Smith' },
-  { id: 3, name: 'Budd Deey' },
-];
-
-const QueryType = new GraphQLObjectType({
-  name: 'Query',
-  fields: {
-    people: {
-      type: new GraphQLList(PersonType),
-      resolve: () => peopleData,
-    },
-  },
-});
-
-const MutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {
-    addPerson: {
-      type: PersonType,
-      args: {
-        name: { type: GraphQLString },
-      },
-      resolve: function (_, { name }) {
-        const person = {
-          id: peopleData[peopleData.length - 1].id + 1,
-          name,
-        };
-
-        peopleData.push(person);
-        return person;
-      }
-    },
-  },
-});
-
-const schema = new GraphQLSchema({ query: QueryType, mutation: MutationType });
-
-/*** LINK ***/
-import { graphql, print } from "graphql";
-import { ApolloLink, Observable } from "@apollo/client";
-function delay(wait) {
-  return new Promise(resolve => setTimeout(resolve, wait));
-}
-
-const link = new ApolloLink(operation => {
-  return new Observable(async observer => {
-    const { query, operationName, variables } = operation;
-    await delay(300);
-    try {
-      const result = await graphql({
-        schema,
-        source: print(query),
-        variableValues: variables,
-        operationName,
-      });
-      observer.next(result);
-      observer.complete();
-    } catch (err) {
-      observer.error(err);
-    }
-  });
-});
-
 /*** APP ***/
-import React, { useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
   gql,
+  useBackgroundQuery,
+  useFragment,
   useQuery,
   useMutation,
 } from "@apollo/client";
 import "./index.css";
 
-const ALL_PEOPLE = gql`
-  query AllPeople {
-    people {
+const UPDATE_TRAIL = gql`
+  mutation UpdateTrail($trailId: ID!, $status: TrailStatus!) {
+    setTrailStatus(id: $trailId, status: $status) {
       id
-      name
+      status
     }
   }
 `;
 
-const ADD_PERSON = gql`
-  mutation AddPerson($name: String) {
-    addPerson(name: $name) {
+const TrailFragment = gql`
+  fragment TrailFragment on Trail {
+    name
+    status
+    difficulty
+  }
+`;
+
+const ALL_TRAILS = gql`
+  query allTrails {
+    allTrails {
       id
-      name
     }
   }
+`;
+
+const ALL_TRAILS_WITH_FRAGMENT = gql`
+  query allTrails {
+    allTrails {
+      id
+      ...TrailFragment
+    }
+  }
+  ${TrailFragment}
 `;
 
 function App() {
-  const [name, setName] = useState('');
-  const {
-    loading,
-    data,
-  } = useQuery(ALL_PEOPLE);
-
-  const [addPerson] = useMutation(ADD_PERSON, {
-    update: (cache, { data: { addPerson: addPersonData } }) => {
-      const peopleResult = cache.readQuery({ query: ALL_PEOPLE });
-
-      cache.writeQuery({
-        query: ALL_PEOPLE,
-        data: {
-          ...peopleResult,
-          people: [
-            ...peopleResult.people,
-            addPersonData,
-          ],
-        },
-      });
-    },
-  });
-
   return (
     <main>
-      <h1>Apollo Client Issue Reproduction</h1>
-      <p>
-        This application can be used to demonstrate an error in Apollo Client.
-      </p>
-      <div className="add-person">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={evt => setName(evt.target.value)}
-        />
-        <button
-          onClick={() => {
-            addPerson({ variables: { name } });
-            setName('');
-          }}
-        >
-          Add person
-        </button>
-      </div>
-      <h2>Names</h2>
-      {loading ? (
-        <p>Loading…</p>
-      ) : (
-        <ul>
-          {data?.people.map(person => (
-            <li key={person.id}>{person.name}</li>
-          ))}
-        </ul>
-      )}
+      <TrailsList />
     </main>
   );
 }
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link
-});
+const TrailsList = () => {
+  const { data, loading } = useQuery(ALL_TRAILS);
+  useBackgroundQuery({ query: ALL_TRAILS_WITH_FRAGMENT });
 
+  return (
+    <>
+      <h2>Ski Lifts</h2>
+      {loading ? (
+        <p>Loading…</p>
+      ) : (
+        <ul>
+          {data?.allTrails.map((trail) => (
+            <Trail key={trail.id} id={trail.id} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+const Trail = (props) => {
+  const { data } = useFragment({
+    fragment: TrailFragment,
+    from: {
+      __typename: "Trail",
+      id: props.id,
+    },
+  });
+
+  const [updateTrail] = useMutation(UPDATE_TRAIL, {
+    update: (cache, { data: { setTrailStatus: updateTrailData } }) => {
+      cache.updateFragment(
+        {
+          id: updateTrailData.id,
+          fragment: TrailFragment,
+        },
+        (data) => ({ ...data, status: updateTrailData.status })
+      );
+    },
+  });
+
+  return (
+    <li key={props.id}>
+      {data.name} - {data.status}
+      <input
+        checked={data.status === 'OPEN' ? true : false}
+        type="checkbox"
+        onChange={(e) => {
+          updateTrail({
+            variables: {
+              trailId: props.id,
+              status: e.target.checked ? 'OPEN' : 'CLOSED',
+              }
+            })
+          }
+        }
+      />
+    </li>
+  );
+}
+
+const client = new ApolloClient({
+  uri: 'https://snowtooth.moonhighway.com',
+  cache: new InMemoryCache(),
+});
 const container = document.getElementById("root");
 const root = createRoot(container);
+
 root.render(
   <ApolloProvider client={client}>
     <App />
